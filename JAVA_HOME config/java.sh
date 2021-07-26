a92a747f334df6917d4b100f0c2c0c42383d4b7f
piedpipr <43669876+piedpipr@users.noreply.github.com>
2021-07-26 18:22:04 +0600

    Add files via upload

---
+++ JAVA_HOME config/java.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/usr/lib/jvm/java-11-openjdk-amd64/bin/
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+export J2SDKDIR=/usr/lib/jvm/java-11-openjdk-amd64/
---
+++ JAVA_HOME config/readme.txt
@@ -0,0 +1,1 @@
+put the java.sh file in 'etc/profile.d' folder for terminal to load JAVA_HOME regardless of emulator or user
