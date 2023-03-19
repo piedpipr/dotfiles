64f26392116976980b28c57214cdd1bc56513165
Protik Chanda <43669876+piedpipr@users.noreply.github.com>
2023-03-20 01:16:50 +0600

    Maven added

--- JAVA_HOME config/java.sh
+++ JAVA_HOME config/java.sh
@@ -1,3 +1,8 @@
 export PATH=$PATH:/usr/lib/jvm/java-11-openjdk-amd64/bin/
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 export J2SDKDIR=/usr/lib/jvm/java-11-openjdk-amd64/
+#With maven installed manually
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+export J2SDKDIR=/usr/lib/jvm/java-11-openjdk-amd64/
+export MAVEN_HOME=/opt/apache-maven-3.9.1
+export PATH=$PATH:/usr/lib/jvm/java-11-openjdk-amd64/bin/:$MAVEN_HOME/bin
